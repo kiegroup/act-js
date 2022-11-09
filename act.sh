7b3dafb069d5e758c61fe3f28ffa01fb928fa048
Shubh Bapna <sbapna@redhat.com>
2022-11-09 00:49:22 -0500

    updated script to use act release tags to install act

--- act.sh
+++ act.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/nektos/act/master/install.sh | bash
+if [ $# -eq 0 ]
+then
+  curl https://raw.githubusercontent.com/nektos/act/master/install.sh | bash
+else
+  curl https://raw.githubusercontent.com/nektos/act/master/install.sh | bash -s -- v$1
+fi
 
 mv ./bin/act ./bin/act-js
 
