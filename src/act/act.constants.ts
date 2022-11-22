fa2490692cd8f1fae475c737e00607f955e0b8bf
Shubh Bapna <sbapna@redhat.com>
2022-11-22 18:40:02 -0500

    move act binary in build and execute from there instead of npx

--- src/act/act.constants.ts
+++ src/act/act.constants.ts
@@ -1,7 +1,10 @@
 import { Step } from "@aj/act/act.type";
+import path from "path";
 
 export const DEFAULT_JOB: Step = {
   name: "",
   status: -1,
   output: "",
 };
+
+export const ACT_BINARY = process.env["ACT_BINARY"] ?? path.resolve(__dirname, "..", "..", "bin", "act")
