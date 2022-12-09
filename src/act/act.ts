1eff95108a4a4aa62d93708c7fb0982e30d439d1
Shubh Bapna <38372682+shubhbapna@users.noreply.github.com>
2022-12-09 10:22:32 -0500

    Groups (#5)
    * refactored out extractRunOutput
    
    * implemented a separate class to handle output parsing. added group detection as well
    
    * bump up version and update readme

--- src/act/act.ts
+++ src/act/act.ts
@@ -1,6 +1,6 @@
 import { spawn } from "child_process";
 import { RunOpts, Step, Workflow } from "@aj/act/act.type";
-import { ACT_BINARY, DEFAULT_JOB } from "@aj/act/act.constants";
+import { ACT_BINARY } from "@aj/act/act.constants";
 import { existsSync, writeFileSync } from "fs";
 import path from "path";
 import { homedir } from "os";
@@ -10,6 +10,7 @@ import { StepMocker } from "@aj/step-mocker/step-mocker";
 import { ActionEvent } from "@aj/action-event/action-event";
 import { EventJSON } from "@aj/action-event/action-event.types";
 import { writeFile } from "fs/promises";
+import { OutputParser } from "@aj/output-parser/output-parser";
 
 export class Act {
   private secrets: ArgumentMap;
@@ -241,91 +242,8 @@ export class Act {
       promises.push(proxy.stop());
     }
 
-    const result = this.extractRunOutput(data);
+    const result = new OutputParser(data).parseOutput();
     await Promise.all(promises);
-    return result;
-  }
-
-  /**
-   * Parse the output produced by running act successfully. Produces an object
-   * describing whether the job was successful or not and what was the output of the job
-   * @param output
-   * @returns
-   */
-  private extractRunOutput(output: string): Step[] {
-    // line that has a star followed by Run and job name
-    const runMatcher = /^\s*(\[.+\])\s*\u2B50\s*Run\s*(.*)/;
-    // line that has a green tick mark
-    const successMatcher = /^\s*(\[.+\])\s*\u2705\s*Success\s*-\s*(.*)/;
-    // line that has a red cross
-    const failureMatcher = /^\s*(\[.+\])\s*\u274C\s*Failure\s*-\s*(.*)/;
-    // lines that have no emoji
-    const runOutputMatcher = /^\s*(\[.+\])\s*\|\s*(.*)/;
-
-    // keep track of steps for each job
-    const matrix: Record<string, Record<string, Step>> = {};
-
-    // keep track of the most recent output for a job
-    const matrixOutput: Record<string, string> = {};
-
-    const lines = output.split("\n").map((line) => line.trim());
-    for (const line of lines) {
-      const runMatcherResult = runMatcher.exec(line);
-      const successMatcherResult = successMatcher.exec(line);
-      const failureMatcherResult = failureMatcher.exec(line);
-      const runOutputMatcherResult = runOutputMatcher.exec(line);
-
-      // if the line indicates the start of a step
-      if (runMatcherResult !== null) {
-        // initialize bookkeeping variables
-        if (!matrix[runMatcherResult[1]]) {
-          matrix[runMatcherResult[1]] = {};
-          matrixOutput[runMatcherResult[1]] = "";
-        }
-
-        // create a step object
-        matrix[runMatcherResult[1]][runMatcherResult[2].trim()] = {
-          ...DEFAULT_JOB,
-          name: runMatcherResult[2].trim(),
-        };
-      }
-      // if the line indicates that a step was successful
-      else if (successMatcherResult !== null) {
-        // store output in step
-        matrix[successMatcherResult[1]][successMatcherResult[2].trim()] = {
-          ...matrix[successMatcherResult[1]][successMatcherResult[2].trim()],
-          status: 0,
-          output: matrixOutput[successMatcherResult[1]].trim(),
-        };
-
-        // reset output
-        matrixOutput[successMatcherResult[1]] = "";
-      }
-      // if the line indicates that a step failed
-      else if (failureMatcherResult !== null) {
-        // store output in step
-        matrix[failureMatcherResult[1]][failureMatcherResult[2].trim()] = {
-          ...matrix[failureMatcherResult[1]][failureMatcherResult[2].trim()],
-          status: 1,
-          output: matrixOutput[failureMatcherResult[1]].trim(),
-        };
-
-        // reset output
-        matrixOutput[failureMatcherResult[1]] = "";
-      }
-      // if the line is an output line
-      else if (runOutputMatcherResult !== null) {
-        matrixOutput[runOutputMatcherResult[1]] +=
-          runOutputMatcherResult[2] + "\n";
-      }
-    }
-
-    let result: Step[] = [];
-    Object.values(matrix).forEach((jobs) => {
-      Object.values(jobs).forEach((step) => {
-        result.push(step);
-      });
-    });
     return result;
   }
 
