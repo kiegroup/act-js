5133ac020c07e7c047dc2d6081a54ccf2a30e205
Shubh Bapna <38372682+shubhbapna@users.noreply.github.com>
2023-01-20 09:59:07 -0500

    Improve publish (#8)
    * improved workflows
    
    * updated test cases

--- test/ci/publish.test.ts
+++ test/ci/publish.test.ts
@@ -1,10 +1,129 @@
 import { Act } from "@aj/act/act";
 import { MockGithub } from "@kie/mock-github";
+import axios from "axios";
+import { readFileSync, writeFileSync } from "fs";
+import { rm } from "fs/promises";
 import path from "path";
 
-let github: MockGithub;
-beforeEach(async () => {
-  github = new MockGithub({
+
+describe("no version change", () => {
+  let github: MockGithub;
+
+  beforeEach(async () => {
+    const json = await axios.get("https://unpkg.com/@kie/act-js@latest/package.json");
+    const version = json.data.version;
+    github = await setup(version);
+  })
+
+  afterEach(async () => teardown(github));
+
+  test("publish workflow - no version change", async () => {
+    const act = new Act(github.repo.getPath("publish"));
+    const result = await act.setSecret("NPM_TOKEN", "fake_token").runJob("build");
+  
+    expect(result).toMatchObject([
+      {
+        name: "Main actions/checkout@v3",
+        status: 0,
+        output: "",
+      },
+      {
+        name: "Main actions/setup-node@v3",
+        output: expect.any(String),
+        status: 0,
+      },
+      {
+        name: "Main check for version change",
+        status: 0,
+        output: expect.any(String),
+      },
+      {
+        name: 'Main echo "No version change detected in package.json. Won\'t publish"',
+        status: 0,
+        output: "No version change detected in package.json. Won't publish",
+      },
+      {
+        name: "Post actions/setup-node@v3",
+        output: "",
+        status: 0,
+      },
+    ]);
+  });
+})
+
+describe("version change", () => {
+  let github: MockGithub;
+
+  beforeEach(async () => {
+    github = await setup("999.999.999");
+  })
+
+  afterEach(async () => teardown(github));
+
+  test("publish workflow - version change", async () => {
+    const act = new Act(github.repo.getPath("publish"));
+    const result = await act
+      .setSecret("NPM_TOKEN", "fake_token")
+      .runJob("build", {
+        mockSteps: {
+          build: [
+            {
+              run: "npm publish --access public",
+              mockWith:
+                "echo Making sure that npm token is set - $NODE_AUTH_TOKEN",
+            },
+          ],
+        },
+      });
+  
+    expect(result).toMatchObject([
+      {
+        name: "Main actions/checkout@v3",
+        status: 0,
+        output: "",
+      },
+      {
+        name: "Main actions/setup-node@v3",
+        output: expect.any(String),
+        status: 0,
+      },
+      {
+        name: "Main check for version change",
+        status: 0,
+        output: expect.any(String),
+      },
+      {
+        name: "Main npm ci",
+        status: 0,
+        output: expect.any(String),
+      },
+      {
+        name: "Main npm run build",
+        status: 0,
+        output: expect.stringMatching(/tsc && tsc-alias/),
+      },
+      {
+        name: "Main echo Making sure that npm token is set - $NODE_AUTH_TOKEN",
+        status: 0,
+        output: "Making sure that npm token is set - ***",
+      },
+      {
+        name: "Post actions/setup-node@v3",
+        output: "",
+        status: 0,
+      },
+    ]);
+  });
+})
+
+async function setup(version: string) {
+  const packageJson = readFileSync(
+    path.resolve(__dirname, "..", "..", "package.json"),
+    "utf8"
+  ).replace(/"version":\s"\d\.\d\.\d"/, `"version":"${version}"`);
+  writeFileSync(path.join(__dirname, "package.json"), packageJson);
+  
+  const github = new MockGithub({
     repo: {
       publish: {
         files: [
@@ -13,7 +132,7 @@ beforeEach(async () => {
             dest: ".github",
           },
           {
-            src: path.resolve(__dirname, "..", "..", "package.json"),
+            src: path.join(__dirname, "package.json"),
             dest: "package.json",
           },
           {
@@ -36,62 +155,11 @@ beforeEach(async () => {
       },
     },
   });
+  
   await github.setup();
-});
+  return github;
+}
 
-afterEach(async () => {
-  await github.teardown();
-});
-
-test("publish workflow", async () => {
-  const act = new Act(github.repo.getPath("publish"));
-  const result = await act.setSecret("NPM_TOKEN", "fake_token").runJob("build", {
-    mockSteps: {
-      build: [
-        {
-          run: "npm publish --access public",
-          mockWith:
-            "echo Making sure that npm token is set - $NODE_AUTH_TOKEN",
-        },
-      ],
-    },
-  });
-
-  expect(result).toMatchObject([
-    {
-      name: "Main actions/checkout@v3",
-      status: 0,
-      output: "",
-    },
-    {
-      name: "Main actions/setup-node@v3",
-      output: expect.any(String),
-      status: 0,
-    },
-    {
-      name: "Main npm install",
-      status: 0,
-      output: expect.any(String),
-    },
-    {
-      name: "Main ./scripts/act.sh $VERSION",
-      status: 0,
-      output: expect.any(String),
-    },
-    {
-      name: "Main npm run build",
-      status: 0,
-      output: expect.stringMatching(/tsc && tsc-alias/),
-    },
-    {
-      name: "Main echo Making sure that npm token is set - $NODE_AUTH_TOKEN",
-      status: 0,
-      output: "Making sure that npm token is set - ***",
-    },
-    {
-      name: "Post actions/setup-node@v3",
-      output: "",
-      status: 0,
-    },
-  ]);
-});
+async function teardown(github: MockGithub) {
+  await Promise.all([github.teardown(), rm(path.join(__dirname, "package.json"))]);
+}
