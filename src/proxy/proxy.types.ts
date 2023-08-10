cea009b0b11d187e9f02b0d65ad2f71836b9dc54
Shubh Bapna <sbapna@redhat.com>
2023-08-10 14:29:06 -0400

    handle clients which send a connect request for http destinations

--- src/proxy/proxy.types.ts
+++ src/proxy/proxy.types.ts
@@ -2,6 +2,7 @@ import { Moctokit } from "@kie/mock-github";
 import { Mockapi  } from "@aj/mockapi/mockapi";
 
 export type ResponseMocker = ReturnType<typeof Mockapi.prototype.mock["any"]["any"]["any"]> | ReturnType<Extract<typeof Moctokit.prototype.rest>>;
+export const LOCALHOST = "127.0.0.1";
 
 type Extract<T extends typeof Moctokit.prototype.rest> = {
   [K in keyof T]:  {
