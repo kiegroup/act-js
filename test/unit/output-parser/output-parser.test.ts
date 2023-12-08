ee6cca853c00231f9fcfd666e9c4b753dbb2bbcb
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-12-08 13:57:05 +0000

    Add tests for #60 and interleaved groups

--- test/unit/output-parser/output-parser.test.ts
+++ test/unit/output-parser/output-parser.test.ts
@@ -41,5 +41,71 @@ describe("parseOutput", () => {
         }
       ]);
     });
+
+    test("interleaved groups", async () => {
+      const output = fs.readFileSync(path.join(resources, "act-groups-interleaved.log")).toString();
+      const parser = new OutputParser(output);
+
+      const result = parser.parseOutput();
+
+      expect(result).toStrictEqual([
+        {
+          "groups": [
+            {
+              "name": "Unit Tests",
+              "output": "1231 tests executed."
+            },
+            {
+              "name": "Integration Tests",
+              "output": "32 tests executed."
+            }
+          ],
+          "name": "Main Testing",
+          "output": "1231 tests executed.\n32 tests executed.",
+          "status": 0
+        },
+        {
+          "groups": [
+            {
+              "name": "Lint",
+              "output": "No linting errors found."
+            }
+          ],
+          "name": "Main Lint",
+          "output": "No linting errors found.",
+          "status": 0
+        }
+      ]);
+    });
+
+    test("mixed groups", async () => {
+      const output = fs.readFileSync(path.join(resources, "act-groups-mixed.log")).toString();
+      const parser = new OutputParser(output);
+  
+      const result = parser.parseOutput();
+  
+      expect(result).toStrictEqual([
+        {
+          "groups": [
+            {
+              "name": "Unit Tests",
+              "output": "1231 tests executed."
+            },
+            {
+              "name": "Integration Tests",
+              "output": "32 tests executed."
+            }
+          ],
+          "name": "Main Testing",
+          "output": "1231 tests executed.\n32 tests executed.",
+          "status": 0
+        },
+        {
+          "name": "Main Lint",
+          "output": "No problems found.",
+          "status": 0
+        }
+      ]);
+    });
   });
 });
