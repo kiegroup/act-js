2938efc32a243fe5d5f453e81e88d2ca07a01cec
Marek Novotný <hotmana76@gmail.com>
2026-03-30 16:31:00 +0200

    fixed: ci tests

--- test/ci/pull_request.test.ts
+++ test/ci/pull_request.test.ts
@@ -66,22 +66,23 @@ test("pull request workflow", async () => {
   });
 
   expect(result).toMatchObject([
-    {
-      name: "Main actions/checkout@v4",
-      status: 0,
-      output: "",
-    },
-    {
-      name: "Main actions/setup-node@v4",
-      output: expect.any(String),
-      status: 0,
-    },
-    { name: "Main Install packages", status: 0, output: expect.any(String) },
-    { name: "Main Test", status: 0, output: "ran tests" },
-    {
-      name: "Post actions/setup-node@v4",
-      output: "",
-      status: 0,
-    }
-  ]);
+  {
+    name: "Main actions/checkout@v4",
+    status: 0,
+    output: "",
+  },
+  {
+    name: "Main actions/setup-node@v4",
+    output: expect.any(String),
+    status: 0,
+  },
+  { name: "Main Install packages", status: 0, output: expect.any(String) },
+  { name: "Main Build package", status: 0, output: expect.any(String) },
+  { name: "Main Test", status: 0, output: "ran tests" },
+  {
+    name: "Post actions/setup-node@v4",
+    output: "",
+    status: 0,
+  }
+]);
 });
